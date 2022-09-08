be50cbf3a8a278a9c10694875f515f0c1fcfe288
Kumsakabeta <yoakin111@yahoo.com>
2022-09-08 13:32:38 -0700

    codding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,17 +4,17 @@
 
 /**
 
-* main - print the string in the put function
+   * main - print the string in the put function
 
-*
+   *
 
-* Description:using the main function
+   * Description:using the main function
 
-* This program prints "Programming is like building a multilingual puzzle
+   * This program prints "Programming is like building a multilingual puzzle
 
-* Return: 0
+   * Return: 0
 
-*/
+   */
 
 int main(void)
 
