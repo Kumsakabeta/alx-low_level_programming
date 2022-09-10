2d1229b5b84c2f4843c0f97f0411d125ac4fce6d
Kumsakabeta <yoakin111@yahoo.com>
2022-09-10 09:27:44 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
 /**
-* main - entry point
-*
-* description:using the main function
-* Return : Always 0 (Sucess)
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
+
 {
 
-  puts("\"Programming is like building a multilingual puzzle")
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
