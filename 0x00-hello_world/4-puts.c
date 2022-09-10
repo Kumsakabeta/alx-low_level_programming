5f9b9091e43ec87081fa92d2561a1d893d8e620f
Kumsakabeta <yoakin111@yahoo.com>
2022-09-10 07:06:33 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+
+
+/**
+
+   * main - print the string in the put function
+
+   *
+
+   * Description: using the main function
+
+   * This program prints "Programming is like building a multilingual puzzle"
+
+   * Return: 0
+
+   */
+
+int main(void)
+
+{
+
+       printf("\nwith the proper grammer,but the outcome is a piece of art");
+
+       return (0);
+
+}
