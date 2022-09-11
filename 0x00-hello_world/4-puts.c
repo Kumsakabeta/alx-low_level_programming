e41d91412ecf28657aedfee48f8601f1dee192fd
Kumsakabeta <yoakin111@yahoo.com>
2022-09-11 08:03:06 -0700

    my puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,15 @@
 
 /**
 
- * main - Entry point
+* main - prints out a string via puts
 
- *
+* Description: Program that prints out the string s followed by a new line
 
- * Return: Always 0 (Success)
+* Return: int 0 when done and no errors
 
- */
+*/
+
+
 
 int main(void)
 
