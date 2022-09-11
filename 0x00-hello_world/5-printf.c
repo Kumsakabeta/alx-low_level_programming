34a7336d696028bb2ee2b3ef878c141fc1ba0d1a
Kumsakabeta <yoakin111@yahoo.com>
2022-09-11 08:40:01 -0700

    my printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,21 @@
-#include<stdio.h>                                                                                                           
+#include <stdio.h>
 
-/**                                                                                                                         
 
-* main - entry point                                                                                                        
 
-*                                                                                                                           
+/**
 
-* description:using the main function                                                                                       
+ * Description: main - Prints out a string to stdout.
 
-* Return : Always 0 (Sucess)                                                                                                
+ * Return: 0 if success.
 
-*/                                                                                                                          
+ */
 
-                                                                                                                            
+int main(void)
 
-int main(void)                                                                                                              
+{
 
-{                                                                                                                           
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-                                                                                                                            
-
-  puts("with the proper grammer,but the outcome is a piece of art,\n");                                                              
-
-  return(0);                                                                                                                
+return (0);
 
 }
