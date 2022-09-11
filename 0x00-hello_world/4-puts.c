e6638c1ce642e9ed7dfcdaeb760ada680e8b9b38
Kumsakabeta <yoakin111@yahoo.com>
2022-09-11 09:46:14 -0700

    my puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
-
-
-
 /**
-
 * main - prints out a string via puts
-
+*
 * Description: Program that prints out the string s followed by a new line
-
-* Return: int 0 when done and no errors
-
+* Return: if Sucess
 */
-
-
-
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 
-	return (0);
+return (0);
 
 }
+
