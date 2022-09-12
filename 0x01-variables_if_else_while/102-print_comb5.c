701f3027285734372a7ca03723efb5eb6c5ee13a
Kumsakabeta <yoakin111@yahoo.com>
2022-09-12 06:27:35 -0700

    102_print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- * a comma followed by a space, in ascending order.
- *
- * Return: Always 0.
- */
-
+* main - Prints the numbers from 00 to 99, numbers separated by
+* a comma followed by a space, in ascending order.
+* Return: Always 0.
+*/
 int main(void)
 
 {
