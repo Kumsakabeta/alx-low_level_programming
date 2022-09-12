26beb5801f9a24713b16e496ef41ecc46c1c2d9c
Kumsakabeta <yoakin111@yahoo.com>
2022-09-12 06:18:55 -0700

    102_print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<string.h>
+
+#define N 10
+
+ 
+
+void print(int *num, int n)
+
+{
+
+    int i;
+
+    for ( i = 0 ; i < n ; i++)
+
+        printf("%d ", num[i]);
+
+    printf("\n");
