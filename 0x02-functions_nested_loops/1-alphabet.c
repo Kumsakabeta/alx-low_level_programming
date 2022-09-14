dde6c9eb37556d8bff577284c33f47a4d5ad69d7
Kumsakabeta <yoakin111@yahoo.com>
2022-09-14 15:48:16 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,18 +4,14 @@
 *
 * Return: Always 0.
 */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
-char c;
-for(c='a';c<='z';c++)
+char c='a';
+while(c<='z')
 
 {
 _putchar(c);
+c++;
 }
 _putchar('\n');
 }
