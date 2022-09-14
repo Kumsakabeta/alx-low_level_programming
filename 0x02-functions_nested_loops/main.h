d41d67a8afa6782c74df49f9b2efc1d96450cb49
Kumsakabeta <yoakin111@yahoo.com>
2022-09-14 16:14:49 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,26 @@
 #include "main.h"
-/**
- *
- * * main - print putchar
- *
- * * return :always 0
- *
- * */
+int_putchar(char);
 
-int main(void)
+void print_alaphabet(void);
 
-{
+void print_alphabet_x10(void)
 
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+int _islower(int c);
+int _isalpha(int c);
 
+int print_sign(int n);
 
+int _abs(int);
 
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+	
+void times_table(void);
+
+	
+int add(int, int);
+
+void print_to_98(int n);
+#endif
