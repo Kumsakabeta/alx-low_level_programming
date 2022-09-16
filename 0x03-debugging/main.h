0f17c3cce3c3fc1859dba79ddfe5f85044b3c759
Kumsakabeta <yoakin111@yahoo.com>
2022-09-15 17:35:18 -0700

    debuging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int , int ,int);
 void print_remaining_daya(int ,int ,int);
 int convert_days(int ,int);
+int putchar(int char);
 #endif
