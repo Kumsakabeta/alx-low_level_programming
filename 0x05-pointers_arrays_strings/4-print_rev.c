7e22af2f652db4c0138cafa289be707a20789db1
Kumsakabeta <yoakin111@yahoo.com>
2022-09-19 14:48:55 -0700

    4 print reve

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-
 /**
 * print_rev -> print a string revers
 *
@@ -10,9 +9,9 @@
 void print_rev(char *s)
 {
 int i, n;
+s = "kumsa";
 n = strlen(s);
-
-for (i = n-1;  i >= 0; i--)
+for (i = n - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 
