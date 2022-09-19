6e37372580e679207f66a519a5e0c675b8ddcdfd
Kumsakabeta <yoakin111@yahoo.com>
2022-09-19 07:19:10 -0700

    swaping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int -> given two integers swap the values
+* * @a: [arameter 1
+* * @b: parameter 2
+*/
+
+void swap_int(int *a, int *b)
+{
+int tmp;
+
+tmp = *a;
+
+*a = *b;
+*b = *tmp;
+
+}
