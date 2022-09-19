2ef27f7d6a77092dd234f3c905156104f08fd419
Kumsakabeta <yoakin111@yahoo.com>
2022-09-19 13:35:20 -0700

    print _rev

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * main - check the code
 *
-*  Return: Alway
+* description:the puts function
+*
+*  Return: Always 0 (Sucess)
 */
 void _puts(char *str)
 {
@@ -11,7 +14,5 @@ for (; *str != '\0'; str++)
 _putchar(*str);
 
 }
-
 _putchar('\n');
-
 }
