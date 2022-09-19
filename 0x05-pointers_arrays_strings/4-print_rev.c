ec2ae60fbf649b1480fa3a7f89018460fed1cbca
Kumsakabeta <yoakin111@yahoo.com>
2022-09-19 14:24:28 -0700

    4 print reve

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,20 @@
 #include "main.h"
+#include <string.h>
+
 /**
+* print_rev -> print a string revers
 *
-*   main - check the code
+*  @s :the string to be printed
 *
-*   Return: Always 0.
 */
-
-int main(void)
+void print_rev(char *s)
 {
-char *str;
+int i, n;
+n = strlen(s);
 
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+for (i = n-1;  i >= 0; i--)
+{
+_putchar(s[i]);
 
-print_rev(str)
-return (0);
+}
 }
