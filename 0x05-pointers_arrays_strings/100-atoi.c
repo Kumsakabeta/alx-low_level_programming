50609379716d9b63e4203ec2dd1a392adaf85efd
Kumsakabeta <yoakin111@yahoo.com>
2022-09-20 08:03:08 -0700

    100atoic

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+*  _atoi - convers a string to an int
+*
+*  @s: a pointer to a string guven by main
+*
+*  Description: making our own atoi function
+*
+* Return: returns 0 if no numbers and the result if numbers
+*/
+
+
+int _atoi(char *s)
+
+{
+
+int counter = 0;/* A */
+
+int i = 0;
+
+unsigned int result = 0;
+
+int sign = 0;
+
+while (s[counter] != '\0')/* B */
+
+	{
+
+if (s[counter] == '-')/* C */
+
+sign++;
+
+if (s[counter] >= 48 && s[counter] <= 57)/* D */
+
+{
+
+i++;
+
+result = result * 10 + s[counter] - '0';/* E */
+
+}
+
+if (i > 0 && (s[counter] < 48 || s[counter] > 57))/* F */
+
+break;
+
+counter++;/* G */
+	}
