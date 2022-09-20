52dde0ac3e463440a97d9538c0be733a52809d48
Kumsakabeta <yoakin111@yahoo.com>
2022-09-19 17:08:47 -0700

    6 puts

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
-* _strlen -> function to get the length of a string
-* description :returns the length of a string
-* @s:string pointer to passed this function
-* return:returns the length of the string
+* _strlen - function to get the length of a string
+*  @s:string pointer to passed this function
+*
 */
+
 int _strlen(char *s)
 {
 return (strlen(s));
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+* puts2 - > print a string revers
+* * @str :str[arameter
+*
+*/
+
+void puts2(char *str)
+
+{
+
+char tmp;
+
+int len;
+int i;
+
+len = strlen(str)
+
+for (i - 0; i < len; i++)
+{
+_putchar(str[i])
+_putchar('\n');
+
+}
+}
